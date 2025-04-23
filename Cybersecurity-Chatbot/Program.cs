180973f59c8edfee3de6af3af4a04ba1708fc3a5
ST10454205 <ST10454205@vcconnect.edu.za>
2025-04-23 17:07:40 +0200

    I have set up my methods to play my audio file and display my ASCII art

--- Cybersecurity-Chatbot/Program.cs
+++ Cybersecurity-Chatbot/Program.cs
@@ -1,16 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Media;
 
 namespace Cybersecurity_Chatbot
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            PlayVoiceGreeting();
+            AsciiArt();
+         
+            
         }
+
+        // Method to play a voice greeting
+
+        static void PlayVoiceGreeting()
+        {
+            SoundPlayer player = new SoundPlayer(Properties.Resources.Chat_bot_greeting);
+            player.Play();
+        }
+
+        // Method to display ASCII art
+        static void AsciiArt()
+        {
+            string asciiArt = @"
+  ██████╗██╗   ██╗██████╗ ███████╗██████╗     ███████╗ █████╗ ███████╗███████╗████████╗██╗   ██╗
+██╔════╝╚██╗ ██╔╝██╔══██╗██╔════╝██╔══██╗    ██╔════╝██╔══██╗██╔════╝██╔════╝╚══██╔══╝╚██╗ ██╔╝
+██║      ╚████╔╝ ██████╔╝█████╗  ██████╔╝    ███████╗███████║█████╗  █████╗     ██║    ╚████╔╝ 
+██║       ╚██╔╝  ██╔══██╗██╔══╝  ██╔══██╗    ╚════██║██╔══██║██╔══╝  ██╔══╝     ██║     ╚██╔╝  
+╚██████╗   ██║   ██████╔╝███████╗██║  ██║    ███████║██║  ██║██║     ███████╗   ██║      ██║   
+ ╚═════╝   ╚═╝   ╚═════╝ ╚══════╝╚═╝  ╚═╝    ╚══════╝╚═╝  ╚═╝╚═╝     ╚══════╝   ╚═╝      ╚═╝                                                                                            
+                                   _______________________
+                                  ||                     ||
+                                  ||                     ||
+                                  ||                     ||
+                                  ||                     ||
+                                   \\                   //
+                                    \\                 //
+                                     \\               //
+                                      \\             //
+                                       \\           //
+                                        \\         //
+                                         \\       //
+                                          \\     //  
+                                           \\   //
+                                            \\ //
+                                             \V/
+         
+
+                            >>> Cybersecurity Awareness Bot <<<
+";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(asciiArt);
+            Console.ResetColor();
+        }
+
+       
     }
 }
+
+
